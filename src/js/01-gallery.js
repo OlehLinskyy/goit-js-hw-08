9fc163c74ca2a026a60e19a1c25174e010335486
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-03-30 19:44:51 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const builtItem = ({ description, preview, original }) => {
+  return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+    </li>`;
+};
+const markup = galleryItems.map(builtItem).join("");
+gallery.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
